16f0309494d6e7bc6681a79e4781f2611916e787
“583345555@qq.com” <58334@LAPTOP-U21AEU75>
2021-11-19 19:36:18 +0800

    添加项目文件。

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.freePalnt = new System.Windows.Forms.Button();
+            this.Kill = new System.Windows.Forms.Button();
+            this.plantnocd = new System.Windows.Forms.Button();
+            this.shorttime = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.select = new System.Windows.Forms.Button();
+            this.getmoney = new System.Windows.Forms.Button();
+            this.recover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "无限阳光";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SUN_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "自动拾取阳光";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Autosunn_Click);
+            // 
+            // freePalnt
+            // 
+            this.freePalnt.Location = new System.Drawing.Point(27, 149);
+            this.freePalnt.Name = "freePalnt";
+            this.freePalnt.Size = new System.Drawing.Size(116, 32);
+            this.freePalnt.TabIndex = 6;
+            this.freePalnt.Text = "免费种植物";
+            this.freePalnt.UseVisualStyleBackColor = true;
+            this.freePalnt.Click += new System.EventHandler(this.freePalnt_Click);
+            // 
+            // Kill
+            // 
+            this.Kill.Location = new System.Drawing.Point(169, 199);
+            this.Kill.Name = "Kill";
+            this.Kill.Size = new System.Drawing.Size(116, 35);
+            this.Kill.TabIndex = 8;
+            this.Kill.Text = "秒杀僵尸";
+            this.Kill.UseVisualStyleBackColor = true;
+            this.Kill.Click += new System.EventHandler(this.Kill_Click);
+            // 
+            // plantnocd
+            // 
+            this.plantnocd.Location = new System.Drawing.Point(169, 149);
+            this.plantnocd.Name = "plantnocd";
+            this.plantnocd.Size = new System.Drawing.Size(116, 32);
+            this.plantnocd.TabIndex = 9;
+            this.plantnocd.Text = "植物无冷却";
+            this.plantnocd.UseVisualStyleBackColor = true;
+            this.plantnocd.Click += new System.EventHandler(this.plantnocd_Click);
+            // 
+            // shorttime
+            // 
+            this.shorttime.Location = new System.Drawing.Point(27, 202);
+            this.shorttime.Name = "shorttime";
+            this.shorttime.Size = new System.Drawing.Size(116, 32);
+            this.shorttime.TabIndex = 10;
+            this.shorttime.Text = "缩短冷却时间";
+            this.shorttime.UseVisualStyleBackColor = true;
+            this.shorttime.Click += new System.EventHandler(this.shorttime_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 38);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(302, 37);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(114, 38);
+            this.select.TabIndex = 12;
+            this.select.Text = "选择关卡";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // getmoney
+            // 
+            this.getmoney.Location = new System.Drawing.Point(27, 254);
+            this.getmoney.Name = "getmoney";
+            this.getmoney.Size = new System.Drawing.Size(116, 36);
+            this.getmoney.TabIndex = 13;
+            this.getmoney.Text = "无限金币";
+            this.getmoney.UseVisualStyleBackColor = true;
+            this.getmoney.Click += new System.EventHandler(this.getmoney_Click);
+            // 
+            // recover
+            // 
+            this.recover.Location = new System.Drawing.Point(169, 254);
+            this.recover.Name = "recover";
+            this.recover.Size = new System.Drawing.Size(116, 36);
+            this.recover.TabIndex = 14;
+            this.recover.Text = "恢复所有修改";
+            this.recover.UseVisualStyleBackColor = true;
+            this.recover.Click += new System.EventHandler(this.recover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 313);
+            this.Controls.Add(this.recover);
+            this.Controls.Add(this.getmoney);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.shorttime);
+            this.Controls.Add(this.plantnocd);
+            this.Controls.Add(this.Kill);
+            this.Controls.Add(this.freePalnt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.SUN_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button freePalnt;
+        private System.Windows.Forms.Button Kill;
+        private System.Windows.Forms.Button plantnocd;
+        private System.Windows.Forms.Button shorttime;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.Button getmoney;
+        private System.Windows.Forms.Button recover;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+
+        private void SUN_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            //获取无限阳光
+            int address = ReadMemoryValue(baseAddress);            
+            address = address + 0x768;                              
+            address = ReadMemoryValue(address);
+            address = address + 0x5560;                            
+            WriteMemory(address, 0x270F);                      
+            
+        }
+        private void getmoney_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            //获取无限金币
+            int address = ReadMemoryValue(baseAddress);
+            address = address + 0x82C;
+            address = ReadMemoryValue(address);
+            address = address + 0x28;
+            WriteMemory(address, 0x270F);
+
+        }
+        private void Autosunn_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            //自动拾取阳光
+            byte[] data = { 0xEB };
+            Helper.WritetoMemoryValue(0x43158f, processName, data);
+        }
+
+        private void freePalnt_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            // 免费种植物
+            byte[] data = { 0x90, 0x90 };
+            Helper.WritetoMemoryValue(0x41BA74, processName, data);
+        }
+
+        private void Kill_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            // 秒杀普通僵尸
+            //53130F 2B7C2420   sub edi,dword ptr ss:[esp+20]--->33FF 9090 xor edi,edi nop nop
+            byte[] data1 = { 0x33, 0xFF, 0x90, 0x90 };
+            Helper.WritetoMemoryValue(0x53130F, processName, data1);
+            byte[] data2 = { 0x90, 0x90 };
+            //秒杀不普通僵尸
+            //0x53186D  7E 08-->0x90 0x90
+            //0x531858 7E  1D--->0x90 0x90
+            Helper.WritetoMemoryValue(0x53186D, processName, data2); 
+            Helper.WritetoMemoryValue(0x531858, processName, data2);
+
+
+        }
+        private void plantnocd_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            //植物无冷却
+            byte[] data = { 0x75, 0x20 };
+            //0x48728A 74 20 je --->75 20 jne
+            Helper.WritetoMemoryValue(0x48728A, processName, data);
+
+        }
+        private void shorttime_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            //缩短植物冷却时间
+            byte[] data = { 0x83, 0x47, 0x24, 0x50 };
+            //0x48728c 8347 24 01 add dword ptr ds:[edi+24],1 --->834724  50 add dword ptr ds:[edi+24],0x50
+            Helper.WritetoMemoryValue(0x48728c, processName, data);
+
+        }
+        private void select_Click(object sender, EventArgs e)
+        {
+            if (Helper.GetPidByProcessName(processName) == 0)
+            {
+                MessageBox.Show("运行辅助前请先运行游戏！");
+                return;
+            }
+            int address = ReadMemoryValue(baseAddress);          
+            address = address + 0x82C;                             
+            address = ReadMemoryValue(address);
+            address = address + 0x24;
+            int lev = 1;
+            try
+            {
+                lev = int.Parse(textBox1.Text.Trim());
+                Console.WriteLine(lev);
+            }
+            catch
+            {
+                MessageBox.Show("输入格式不正确！");
+            }
+
+            WriteMemory(address, lev);
+
+        }
+        private void recover_Click(object sender, EventArgs e)
+        {
+            //植物冷却时间
+
+            byte[] data1 = { 0x83, 0x47, 0x24, 0x01 };
+            Helper.WritetoMemoryValue(0x48728c, processName, data1);
+            //植物无冷却
+            byte[] data2 = { 0x74, 0x20 };
+            //0x48728A 74 20 je --->75 20 jne
+            Helper.WritetoMemoryValue(0x48728A, processName, data2);
+            // 秒杀普通僵尸
+            //53130F 2B7C2420   sub edi,dword ptr ss:[esp+20]--->33FF 9090 xor edi,edi nop nop
+            byte[] data3 = { 0x2B, 0x7C, 0x24, 0x20 };
+            Helper.WritetoMemoryValue(0x53130F, processName, data3);
+            byte[] data4 = { 0x7E, 0x08 };
+            byte[] data5 = { 0x7E, 0x1D };
+            //秒杀不普通僵尸
+            //0x53186D  7E 08-->0x90 0x90
+            //0x531858 7E  1D--->0x90 0x90
+            Helper.WritetoMemoryValue(0x53186D, processName, data4);
+            Helper.WritetoMemoryValue(0x531858, processName, data5);
+            // 免费种植物
+            byte[] data6 = { 0x2B, 0xF3 };
+            Helper.WritetoMemoryValue(0x41BA74, processName, data6);
+            //自动拾取阳光
+            byte[] data7 = { 0x75 };
+            Helper.WritetoMemoryValue(0x43158f, processName, data7);
+
+
+
+        }
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("[1]在主菜单页面，开始关卡之前设置，进入关卡后点击重玩游戏\n[2]在游戏中途想改关卡，可以在游戏进行中设置目标关卡,点击重玩游戏，或者回退到主菜单\n[3]关卡默认值为1\n");
+
+        }
+
+
+        public int ReadMemoryValue(int baseAdd)
+        {
+            return Helper.ReadMemoryValue(baseAdd, processName);
+        }
+
+        
+        public void WriteMemory(int baseAdd, int value)
+        {
+            Helper.WriteMemoryValue(baseAdd, processName, value);
+        }
+
+        private int baseAddress = 0x006A9EC0;           //游戏内存基址
+        private string processName = "PlantsVsZombies"; //游戏进程名字
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            // 创建the ToolTip 
+            ToolTip toolTip1 = new ToolTip();
+
+            // 设置显示样式
+            toolTip1.AutoPopDelay = 5000;//提示信息的可见时间
+            toolTip1.InitialDelay = 500;//事件触发多久后出现提示
+            toolTip1.ReshowDelay = 500;//指针从一个控件移向另一个控件时，经过多久才会显示下一个提示框
+            toolTip1.ShowAlways = true;//是否显示提示框
+
+            //  设置伴随的对象.
+            
+            toolTip1.SetToolTip(this.select, "设置关卡之后点击");
+            toolTip1.SetToolTip(this.plantnocd, "在刚进入游戏，所有卡片第一次冷却时间都过了之后点击");
+        }
+
+       
+    }
+}
